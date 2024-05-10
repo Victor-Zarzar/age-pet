123754e9f4c2fcea94911925798c14ed477ccd53
victorzarzar <victorzarzar58@gmail.com>
2024-05-10 15:35:08 -0300

    Refactor: CatPage, DogPage

--- lib/pages/CatPage/cat_page.dart
+++ lib/pages/CatPage/cat_page.dart
@@ -1,5 +1,7 @@
-import "package:age_pet/components/AppTheme/app_theme.dart";
-import "package:flutter/material.dart";
+import 'package:age_pet/components/AppTheme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class CatPage extends StatefulWidget {
   const CatPage({super.key});
@@ -9,6 +11,10 @@ class CatPage extends StatefulWidget {
 }
 
 class _CatPageState extends State<CatPage> {
+  TextEditingController humanAgeController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  int? felineAge;
+
   @override
   Widget build(BuildContext context) {
     double myHeight = MediaQuery.of(context).size.height;
@@ -21,15 +27,65 @@ class _CatPageState extends State<CatPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image.asset('lib/assets/cat.png',
-            height: myHeight * 0.15,
-            width: myWidth * 0.40,
+            Image.asset(
+              'lib/assets/cat.png',
+              height: myHeight * 0.15,
+              width: myWidth * 0.40,
             ),
             const SizedBox(height: 20),
-            const Text('Idade do Gato'),
+            Text(
+              'Idade do Gato',
+              style: GoogleFonts.jetBrainsMono(),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: humanAgeController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Idade Humana',
+              ),
+            ),
+            TextFormField(
+              controller: weightController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Peso (kg)',
+              ),
+            ),
+            const SizedBox(height: 10),
+            GFButton(
+              onPressed: () {
+                setState(() {
+                  int humanAge = int.parse(humanAgeController.text);
+                  double weight = double.parse(weightController.text);
+                  felineAge = calculateFelineAge(humanAge, weight).round();
+                });
+              },
+              color: ButtonColor.primaryColor,
+              shape: GFButtonShape.pills,
+              text: "Calcular",
+              size: GFSize.LARGE,
+            ),
+            if (felineAge != null)
+              Text(
+                'Idade do Gato: $felineAge anos',
+                style: const TextStyle(fontSize: 18),
+              ),
           ],
         ),
       ),
     );
+  }
+
+  double calculateFelineAge(int humanAge, double weight) {
+    double felineAge;
+    double ageFactor = humanAge <= 2 ? 24 : 24 + (humanAge - 2) * 4;
+    double weightFactor = weight <= 5
+        ? 4
+        : weight <= 10
+            ? 6
+            : 8;
+    felineAge = ageFactor + weightFactor * (humanAge - 2);
+    return felineAge;
   }
 }
--- lib/pages/DogPage/dog_page.dart
+++ lib/pages/DogPage/dog_page.dart
@@ -1,5 +1,7 @@
-import "package:age_pet/components/AppTheme/app_theme.dart";
-import "package:flutter/material.dart";
+import 'package:age_pet/components/AppTheme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:getwidget/getwidget.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DogPage extends StatefulWidget {
   const DogPage({super.key});
@@ -9,6 +11,10 @@ class DogPage extends StatefulWidget {
 }
 
 class _DogPageState extends State<DogPage> {
+  TextEditingController humanAgeController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  int? canineAge;
+
   @override
   Widget build(BuildContext context) {
     double myHeight = MediaQuery.of(context).size.height;
@@ -21,15 +27,82 @@ class _DogPageState extends State<DogPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Image.asset('lib/assets/dog.png',
-            height: myHeight * 0.15,
-            width: myWidth * 0.40,
+            Image.asset(
+              'lib/assets/dog.png',
+              height: myHeight * 0.15,
+              width: myWidth * 0.40,
             ),
             const SizedBox(height: 20),
-            const Text('Idade do Cachorro'),
+            Text(
+              'Idade do Cachorro',
+              style: GoogleFonts.jetBrainsMono(),
+            ),
+            const SizedBox(height: 20),
+            TextFormField(
+              controller: humanAgeController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Idade Humana',
+              ),
+            ),
+            TextFormField(
+              controller: weightController,
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                labelText: 'Peso (kg)',
+              ),
+            ),
+            const SizedBox(height: 10),
+            GFButton(
+              onPressed: () {
+                setState(() {
+                  int humanAge = int.parse(humanAgeController.text);
+                  double weight = double.parse(weightController.text);
+                  canineAge = calculateCanineAge(humanAge, weight).round();
+                });
+              },
+              color: ButtonColor.primaryColor,
+              shape: GFButtonShape.pills,
+              text: "Calcular",
+              size: GFSize.LARGE,
+            ),
+            if (canineAge != null)
+              Text(
+                'Idade do Cachorro: $canineAge anos',
+                style: const TextStyle(fontSize: 18),
+              ),
           ],
         ),
       ),
     );
+  }
+
+  double calculateCanineAge(int humanAge, double weight) {
+    double canineAge;
+
+    if (humanAge <= 5) {
+      canineAge = humanAge * 7.2;
+    } else {
+      double ageFactor;
+      double weightFactor;
+
+      if (weight <= 10) {
+        weightFactor = 4;
+      } else if (weight <= 25) {
+        weightFactor = 5;
+      } else {
+        weightFactor = 6;
+      }
+
+      if (humanAge <= 10) {
+        ageFactor = humanAge - 5;
+      } else {
+        ageFactor = 5 + (humanAge - 10) * 0.5;
+      }
+
+      canineAge = 35 + (ageFactor * weightFactor);
+    }
+
+    return canineAge;
   }
 }
