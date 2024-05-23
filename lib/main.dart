601d89a6550e4d95b1ca005aa8db8e855900acd7
victorzarzar <victorzarzar58@gmail.com>
2024-05-23 11:29:52 -0300

    Feat: add awesomenotifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,37 @@
 import 'package:age_pet/pages/IntroPage/intro_page.dart';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
-
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [
-        Locale('en', 'US'),
-        Locale('pt', 'BR'),
-      ],
-      path: 'assets/translations',
-      fallbackLocale: const Locale('en-US'),
-      child: const MyApp(),
-    ),
-  );
+  await AwesomeNotifications().initialize(null, [
+    NotificationChannel(
+        channelGroupKey: "basic_channel_group",
+        channelKey: "basic_channel",
+        channelName: "Basic notification",
+        channelDescription: "Test Notifications channel")
+  ], channelGroups: [
+    NotificationChannelGroup(
+        channelGroupKey: "basic_channel_group", channelGroupName: "Basic Group")
+  ]);
+  bool isAllowedToSendNotification =
+      await AwesomeNotifications().isNotificationAllowed();
+  if (!isAllowedToSendNotification) {
+    AwesomeNotifications().requestPermissionToSendNotifications();
+    runApp(
+      EasyLocalization(
+        supportedLocales: const [
+          Locale('en', 'US'),
+          Locale('pt', 'BR'),
+        ],
+        path: 'assets/translations',
+        fallbackLocale: const Locale('en-US'),
+        child: const MyApp(),
+      ),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
